54b07e1d169787038baa846c6d01c63d940b9f64
Kostya <kostyashtc@gmail.com>
2021-04-07 19:41:06 +0300

    CI/CD: eliminate vulnerability with default password encodder

--- controller/src/main/java/com/epam/esm/config/SecurityConfig.java
+++ controller/src/main/java/com/epam/esm/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(service);
+        auth.userDetailsService(service).passwordEncoder(encoder());
     }
 
     @Override
